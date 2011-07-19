11e89cf6a4a3c217ff152566f00d4a44a9dad052
Forrest Voight <forrest@forre.st>
2011-07-19 12:06:10 -0400

    made dump_addrs.py display age of peers and sort by that age

--- dump_addrs.py
+++ dump_addrs.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import os
 import sqlite3
+import time
 
 from p2pool.util import db
 from p2pool import p2p
@@ -9,12 +10,12 @@ from p2pool import p2p
 print 'main'
 x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs'))
 
-for k, v in x.iteritems():
-    print k, v
+for k, v in sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1]):
+    print k, v, time.time() - v[-1]
 
 print
 print 'testnet'
 x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs_testnet'))
 
-for k, v in x.iteritems():
-    print k, v
+for k, v in sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1]):
+    print k, v, time.time() - v[-1]
