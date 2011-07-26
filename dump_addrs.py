7c815e3252d419b7d34020df06951dacd8de3848
Forrest Voight <forrest@forre.st>
2011-07-26 17:24:48 +0000

    change dump_addrs units to days

--- dump_addrs.py
+++ dump_addrs.py
@@ -10,12 +10,12 @@ from p2pool import p2p
 print 'main'
 x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs'))
 
-for k, v in sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1]):
-    print k, v, time.time() - v[-1]
+for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
+    print i, k, v, (time.time() - v[-1])/24/60/60
 
 print
 print 'testnet'
 x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs_testnet'))
 
-for k, v in sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1]):
-    print k, v, time.time() - v[-1]
+for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
+    print i, k, v, (time.time() - v[-1])/24/60/60
