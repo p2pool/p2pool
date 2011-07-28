f2262d8f96977765bb43ce14d17e5c5eac2458f6
Forrest Voight <forrest@forre.st>
2011-07-28 00:08:16 -0400

    don't try to start your own chain

--- p2pool/main.py
+++ p2pool/main.py
@@ -281,6 +281,8 @@ def main(args):
         run_identifier = struct.pack('<Q', random.randrange(2**64))
         
         def compute(state, all_targets):
+            if state['best_share_hash'] is None:
+                raise jsonrpc.Error(-12345, u"p2pool is downloading shares")
             pre_extra_txs = [tx for tx in tx_pool.itervalues() if tx.is_good()]
             pre_extra_txs = pre_extra_txs[:2**16 - 1] # merkle_branch limit
             extra_txs = []
