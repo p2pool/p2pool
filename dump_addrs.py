7deaef05fcc78c8ffad77e5a189af3994dfb2461
Forrest Voight <forrest@forre.st>
2011-10-08 17:21:16 -0400

    fix dump_addrs

--- dump_addrs.py
+++ dump_addrs.py
@@ -8,14 +8,14 @@ from p2pool.util import db
 from p2pool import p2p
 
 print 'main'
-x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs'))
+x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'bitcoin'))
 
 for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
     print i, k, v, (time.time() - v[-1])/24/60/60
 
 print
 print 'testnet'
-x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'addrs_testnet'))
+x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'bitcoin_testnet'))
 
 for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
     print i, k, v, (time.time() - v[-1])/24/60/60
