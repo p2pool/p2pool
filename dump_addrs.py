5bf38423bcaff3bf4aa931d5082bce2764a42cbe
Forrest Voight <forrest@forre.st>
2011-11-08 19:41:03 -0500

    changed dump_addrs.py to print addresses from all networks

--- dump_addrs.py
+++ dump_addrs.py
@@ -5,17 +5,12 @@ import sqlite3
 import time
 
 from p2pool.util import db
-from p2pool import p2p
+from p2pool import p2p, data
 
-print 'main'
-x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'bitcoin'))
-
-for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
-    print i, k, v, (time.time() - v[-1])/24/60/60
-
-print
-print 'testnet'
-x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), 'bitcoin_testnet'))
-
-for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
-    print i, k, v, (time.time() - v[-1])/24/60/60
+for net_name in data.nets.iterkeys():
+    print
+    print net_name
+    x = p2p.AddrStore(db.SQLiteDict(sqlite3.connect(os.path.join(os.path.dirname(__file__), 'addrs.dat'), isolation_level=None), net_name))
+    
+    for i, (k, v) in enumerate(sorted(x.iteritems(), key=lambda (k, v): time.time() - v[-1])):
+        print i, k, v, (time.time() - v[-1])/24/60/60
