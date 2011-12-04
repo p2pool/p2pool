f0b6984feb48499afc5f69821ea5f33b3b5f7601
Forrest Voight <forrest@forre.st>
2011-12-03 20:05:07 -0500

    removed ixcoin, i0coin, solidcoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -52,71 +52,6 @@ class NamecoinTestnet(networks.NamecoinTestnet):
     PERSIST = False
     WORKER_PORT = 19331
 
-class IxcoinMainnet(networks.IxcoinMainnet):
-    SHARE_PERIOD = 10 # seconds
-    CHAIN_LENGTH = 24*60*60//10 # shares
-    TARGET_LOOKBEHIND = 3600//10 # shares
-    SPREAD = 3 # blocks
-    IDENTIFIER = '27b564116e2a2666'.decode('hex')
-    PREFIX = '9dd6c4a619401f2f'.decode('hex')
-    NAME = 'ixcoin'
-    P2P_PORT = 9335
-    MAX_TARGET = 2**256//2**32 - 1
-    PERSIST = True
-    WORKER_PORT = 9330
-
-class IxcoinTestnet(networks.IxcoinTestnet):
-    SHARE_PERIOD = 1 # seconds
-    CHAIN_LENGTH = 24*60*60//5 # shares
-    TARGET_LOOKBEHIND = 200 # shares
-    SPREAD = 3 # blocks
-    IDENTIFIER = '7430cbeb01249e44'.decode('hex')
-    PREFIX = '7cfffda946709c1f'.decode('hex')
-    NAME = 'ixcoin_testnet'
-    P2P_PORT = 19335
-    MAX_TARGET = 2**256//2**20 - 1
-    PERSIST = False
-    WORKER_PORT = 19330
-
-class I0coinMainnet(networks.I0coinMainnet):
-    SHARE_PERIOD = 10 # seconds
-    CHAIN_LENGTH = 24*60*60//10 # shares
-    TARGET_LOOKBEHIND = 3600//10 # shares
-    SPREAD = 3 # blocks
-    IDENTIFIER = 'b32e3f10c2ff221b'.decode('hex')
-    PREFIX = '6155537ed977a3b5'.decode('hex')
-    NAME = 'i0coin'
-    P2P_PORT = 9336
-    MAX_TARGET = 2**256//2**32 - 1
-    PERSIST = False
-    WORKER_PORT = 9329
-
-class I0coinTestnet(networks.I0coinTestnet):
-    SHARE_PERIOD = 1 # seconds
-    CHAIN_LENGTH = 24*60*60//5 # shares
-    TARGET_LOOKBEHIND = 200 # shares
-    SPREAD = 3 # blocks
-    IDENTIFIER = '7712c1a8181b5f2e'.decode('hex')
-    PREFIX = '792d2e7d770fbe68'.decode('hex')
-    NAME = 'i0coin_testnet'
-    P2P_PORT = 19336
-    MAX_TARGET = 2**256//2**20 - 1
-    PERSIST = False
-    WORKER_PORT = 19329
-
-class SolidcoinMainnet(networks.SolidcoinMainnet):
-    SHARE_PERIOD = 10
-    CHAIN_LENGTH = 24*60*60//10 # shares
-    TARGET_LOOKBEHIND = 3600//10 # shares
-    SPREAD = 3 # blocks
-    IDENTIFIER = '9cc9c421cca258cd'.decode('hex')
-    PREFIX = 'c059125b8070f00a'.decode('hex')
-    NAME = 'solidcoin'
-    P2P_PORT = 9337
-    MAX_TARGET = 2**256//2**32 - 1
-    PERSIST = True
-    WORKER_PORT = 9328
-
 class LitecoinMainnet(networks.LitecoinMainnet):
     SHARE_PERIOD = 10 # seconds
     CHAIN_LENGTH = 24*60*60//10//2 # shares
@@ -143,4 +78,4 @@ class LitecoinTestnet(networks.LitecoinTestnet):
     PERSIST = False
     WORKER_PORT = 19327
 
-nets = dict((net.NAME, net) for net in set([BitcoinMainnet, BitcoinTestnet, NamecoinMainnet, NamecoinTestnet, IxcoinMainnet, IxcoinTestnet, I0coinMainnet, I0coinTestnet, SolidcoinMainnet, LitecoinMainnet, LitecoinTestnet]))
+nets = dict((net.NAME, net) for net in set([BitcoinMainnet, BitcoinTestnet, NamecoinMainnet, NamecoinTestnet, LitecoinMainnet, LitecoinTestnet]))
