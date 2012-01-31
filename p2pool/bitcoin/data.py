a7d3ae4714bb3f8b105a547cc126435f9b133e6f
Forrest Voight <forrest@forre.st>
2012-01-30 23:35:46 -0500

    moved base58 functions into bitcoin.data

--- p2pool/bitcoin/data.py
+++ p2pool/bitcoin/data.py
@@ -190,6 +190,16 @@ def tx_get_sigop_count(tx):
 
 # human addresses
 
+base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def base58_encode(bindata):
+    bindata2 = bindata.lstrip(chr(0))
+    return base58_alphabet[0]*(len(bindata) - len(bindata2)) + bases.natural_to_string(bases.string_to_natural(bindata2), base58_alphabet)
+
+def base58_decode(b58data):
+    b58data2 = b58data.lstrip(base58_alphabet[0])
+    return chr(0)*(len(b58data) - len(b58data2)) + bases.natural_to_string(bases.string_to_natural(b58data2, base58_alphabet))
+
 human_address_type = ChecksummedType(pack.ComposedType([
     ('version', pack.IntType(8)),
     ('pubkey_hash', pack.IntType(160)),
