053d7ca8c1e2930961c9711717d4fd5a48945a65
Forrest Voight <forrest@forre.st>
2012-02-08 14:00:13 -0500

    fixed skiplist unit test, broken since f8646705e

--- p2pool/test/test_skiplists.py
+++ p2pool/test/test_skiplists.py
@@ -10,7 +10,7 @@ class Test(unittest.TestCase):
         t = forest.Tracker()
         d = skiplists.WeightsSkipList(t)
         for i in xrange(200):
-            t.add(test_forest.FakeShare(hash=i, previous_hash=i - 1 if i > 0 else None, new_script=i, donation=1234, target=2**249))
+            t.add(test_forest.FakeShare(hash=i, previous_hash=i - 1 if i > 0 else None, share_data=dict(new_script=i, donation=1234), target=2**249))
         for i in xrange(200):
             a = random.randrange(200)
             d(a, random.randrange(a + 1), 1000000*65535)[1]
