ed8aaf19ff9abc39749207887e36d1e60015d585
Forrest Voight <forrest@forre.st>
2012-10-28 02:07:55 -0400

    fixed p2p.test_tx_limit testcase

--- p2pool/test/test_p2p.py
+++ p2pool/test/test_p2p.py
@@ -36,7 +36,7 @@ class Test(unittest.TestCase):
     def test_tx_limit(self):
         class MyNode(p2p.Node):
             def __init__(self, df):
-                p2p.Node.__init__(self, lambda: None, 29333, networks.nets['litecoin_testnet'], {}, set([('127.0.0.1', 19338)]), 0, 0, 0, 0)
+                p2p.Node.__init__(self, lambda: None, 29333, networks.nets['bitcoin'], {}, set([('127.0.0.1', 9333)]), 0, 0, 0, 0)
                 
                 self.df = df
                 self.sent_time = 0
@@ -44,43 +44,36 @@ class Test(unittest.TestCase):
             @defer.inlineCallbacks
             def got_conn(self, conn):
                 p2p.Node.got_conn(self, conn)
-                huge_tx = dict(
-                    version=0,
-                    tx_ins=[],
-                    tx_outs=[dict(
-                        value=0,
-                        script='x'*900000,
-                    )],
-                    lock_time=0,
-                )
+                
+                yield deferral.sleep(.5)
+                
                 new_mining_txs = dict(self.mining_txs_var.value)
-                new_mining_txs[bitcoin_data.hash256(bitcoin_data.tx_type.pack(huge_tx))] = huge_tx
+                for i in xrange(3):
+                    huge_tx = dict(
+                        version=0,
+                        tx_ins=[],
+                        tx_outs=[dict(
+                            value=0,
+                            script='x'*900000,
+                        )],
+                        lock_time=i,
+                    )
+                    new_mining_txs[bitcoin_data.hash256(bitcoin_data.tx_type.pack(huge_tx))] = huge_tx
                 self.mining_txs_var.set(new_mining_txs)
                 
-                yield deferral.sleep(1)
-                
-                huge_tx = dict(
-                    version=0,
-                    tx_ins=[],
-                    tx_outs=[dict(
-                        value=0,
-                        script='x'*900000,
-                    )],
-                    lock_time=1,
-                )
-                new_mining_txs = dict(self.mining_txs_var.value)
-                new_mining_txs[bitcoin_data.hash256(bitcoin_data.tx_type.pack(huge_tx))] = huge_tx
-                self.mining_txs_var.set(new_mining_txs)
                 self.sent_time = reactor.seconds()
-                
             
             def lost_conn(self, conn, reason):
                 self.df.callback(None)
-        
-        df = defer.Deferred()
-        n = MyNode(df)
-        n.start()
-        yield df
-        if not (n.sent_time <= reactor.seconds() <= n.sent_time + 1):
-            raise ValueError('node did not disconnect within 1 seconds of receiving too much tx data')
-        yield n.stop()
+        try:
+            p2p.Protocol.max_remembered_txs_size *= 10
+            
+            df = defer.Deferred()
+            n = MyNode(df)
+            n.start()
+            yield df
+            if not (n.sent_time <= reactor.seconds() <= n.sent_time + 1):
+                raise ValueError('node did not disconnect within 1 seconds of receiving too much tx data')
+            yield n.stop()
+        finally:
+            p2p.Protocol.max_remembered_txs_size //= 10
