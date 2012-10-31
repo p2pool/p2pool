de39289dfa8e6ab8fa2b482ace51080bb6f4fbf0
Forrest Voight <forrest@forre.st>
2012-10-31 13:17:35 -0400

    improved coverage for VarInt

--- p2pool/test/util/test_pack.py
+++ p2pool/test/util/test_pack.py
@@ -7,3 +7,5 @@ class Test(unittest.TestCase):
         t = pack.VarIntType()
         for i in xrange(2**20):
             assert t.unpack(t.pack(i)) == i
+        for i in xrange(2**36, 2**36+25):
+            assert t.unpack(t.pack(i)) == i
