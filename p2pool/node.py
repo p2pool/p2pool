67a76e69e8eb3ad0e97364d1f9315c3c29d01487
Forrest Voight <forrest@forre.st>
2012-11-26 14:57:17 -0500

    changed shares requested from 501 to 50

--- p2pool/node.py
+++ p2pool/node.py
@@ -109,7 +109,7 @@ class P2PNode(p2p.Node):
                 try:
                     shares = yield peer.get_shares(
                         hashes=[share_hash],
-                        parents=500,
+                        parents=49,
                         stops=list(set(self.node.tracker.heads) | set(
                             self.node.tracker.get_nth_parent_hash(head, min(max(0, self.node.tracker.get_height_and_last(head)[0] - 1), 10)) for head in self.node.tracker.heads
                         ))[:100],
