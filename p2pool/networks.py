7885d9718f1081c42184f2df549fbd50eb4feac6
skralg <skralg@gmail.com>
2013-05-02 10:09:25 -0700

    Added dns seed

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -132,7 +132,7 @@ nets = dict(
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=False,
         WORKER_PORT=19327,
-        BOOTSTRAP_ADDRS=''.split(' '),
+        BOOTSTRAP_ADDRS='i.hashfaster.com'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
