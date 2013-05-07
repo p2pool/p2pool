7a78e5768dbe9b47dd6801ff9f7a5ab0ab72bdbb
narken <narkhaj@gmail.com>
2013-05-07 23:00:14 +0300

    added BBQcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -200,7 +200,7 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
     ),
-        junkcoin=math.Object(
+    junkcoin=math.Object(
         P2P_PREFIX='fbc0b6db'.decode('hex'),
         P2P_PORT=9772,
         ADDRESS_VERSION=16,
@@ -218,6 +218,25 @@ nets = dict(
         ADDRESS_EXPLORER_URL_PREFIX='http://d.evco.in/abe/address/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**12,
+    ),
+    bbqcoin=math.Object(
+        P2P_PREFIX='fde4d942'.decode('hex'),
+        P2P_PORT=19323,
+        ADDRESS_VERSION=85,
+        RPC_PORT=59332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'bbqcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 42*100000000 >> (height + 1)//24000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='BQC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'BBQCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/BBQCoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bbqcoin'), 'bbqcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://bbq.cryptocoinexplorer.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://bbq.cryptocoinexplorer.com/address/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
     ),
     
 )
