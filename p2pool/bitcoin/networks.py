06a38e20c6782b68dc90000485aeb1538adfb423
Rav3nPL <rav3n.pl@gmail.com>
2013-09-28 21:06:48 +0200

    small fix for BTE data

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -296,8 +296,10 @@ nets = dict(
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'bytecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/bytecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bytecoin'), 'bytecoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='http://blockexplorer.bytecoin.in/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://blockexplorer.bytecoin.in/address/',
+	TX_EXPLORER_URL_PREFIX='http://blockexplorer.bytecoin.in/transaction/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
         DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
     ),
 
 )
