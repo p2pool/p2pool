905181f8c964ce39e89df767660f705317cf5887
Rav3nPL <rav3n.pl@gmail.com>
2013-09-30 21:18:47 +0200

    Added ASC AsicCoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -262,7 +262,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
-
+    asiccoin=math.Object(
+        PARENT=networks.nets['asiccoin'],
+        SHARE_PERIOD=30, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=3, # blocks
+        IDENTIFIER='2c80035c7a81bc6f'.decode('hex'),
+        PREFIX='2472ef181efcd37c'.decode('hex'),
+        P2P_PORT=7432,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**32 - 1,
+        PERSIST=False,
+        WORKER_PORT=9433,
+        BOOTSTRAP_ADDRS='japool.com:13432 rav3n.dtdns.net'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-asc',
+        VERSION_CHECK=lambda v: True,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
