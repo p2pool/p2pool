2a4601e7e68d8b2a5afc6db8666dc6cf46f9ce08
Rav3nPL <rav3n.pl@gmail.com>
2013-10-01 22:22:19 +0200

    Added joulecoin XJO support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -280,6 +280,26 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-asc',
         VERSION_CHECK=lambda v: True,
     ),
+    joulecoin=math.Object(
+        PARENT=networks.nets['joulecoin'],
+        SHARE_PERIOD=20, # seconds
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=10, # blocks
+        IDENTIFIER='ac556af4e900ca61'.decode('hex'),
+        PREFIX='16ac009e4fa655ac'.decode('hex'),
+        P2P_PORT=7844,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**32 - 1,
+        PERSIST=False,
+        WORKER_PORT=9844,
+        BOOTSTRAP_ADDRS='rav3n.dtdns.net'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
+
+
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
