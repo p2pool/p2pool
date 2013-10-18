5838280eb81892e5e4087121f1f8bd30a72d44a0
Rav3nPL <rav3n.pl@gmail.com>
2013-10-18 20:31:35 +0200

    Added Un unobtanium support (still testing)

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -343,6 +343,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    unobtanium=math.Object(
+        P2P_PREFIX='03d5b503'.decode('hex'), #messagestart
+        P2P_PORT=65534,
+        ADDRESS_VERSION=130, #pubkey_address
+        RPC_PORT=65535,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'unobtaniumaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 0.001*100000000 if height<2000  else 1*100000000 >> (height * 1)//120000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=180, # s
+        SYMBOL='Un',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'unobtanium') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/unobtanium/') if platform.system() == 'Darwin' else os.path.expanduser('~/.unobtanium'), 'unobtanium.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.andarazoroflove.org/unobtanium/block_crawler.php?block_hash=',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.andarazoroflove.org/unobtanium/block_crawler.php?address=',
+	TX_EXPLORER_URL_PREFIX='http://explorer.andarazoroflove.org/unobtanium/block_crawler.php?transaction=',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -299,6 +299,24 @@ nets = dict(
         VERSION_CHECK=lambda v: True,
     ),
 
+    unobtanium=math.Object(
+        PARENT=networks.nets['unobtanium'],
+        SHARE_PERIOD=30, # seconds
+        CHAIN_LENGTH=12*60*60//30, # shares
+        REAL_CHAIN_LENGTH=12*60*60//30, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=20, # blocks
+        IDENTIFIER='ab0b4afb40b4ca61'.decode('hex'),
+        PREFIX='b6a4b09e04a6504c'.decode('hex'),
+        P2P_PORT=8655,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**32 - 1,
+        PERSIST=False,
+        WORKER_PORT=9655,
+        BOOTSTRAP_ADDRS='rav3n.dtdns.net'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
