62dd88b28d0120009b8df11628b5ee2b2b1d5dfb
Rav3nPL <rav3n.pl@gmail.com>
2013-12-08 20:43:18 +0100

    added DOGE support

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -364,6 +364,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    dogecoin=math.Object(
+        P2P_PREFIX='c0c0c0c0'.decode('hex'),
+        P2P_PORT=22556,
+        ADDRESS_VERSION=30,
+        RPC_PORT=22555,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'dogecoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10000*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='DOGE',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DogeCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Dogecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dogecoin'), 'dogecoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.dogecoin.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.dogecoin.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.dogecoin.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
