82c8c8c09bf5d58e1c9657a8a5535f3802093a8d
Rav3npl <rav3n.pl@gmail.com>
2013-12-22 22:32:55 +0100

    added BIL Billioncoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -320,6 +320,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    billioncoin=math.Object( #565
+        PARENT=networks.nets['billioncoin'],
+        SHARE_PERIOD=15, # seconds target spacing
+        CHAIN_LENGTH=12*60*60//15, # shares
+        REAL_CHAIN_LENGTH=12*60*60//15, # shares
+        TARGET_LOOKBEHIND=20, # shares coinbase maturity
+        SPREAD=10, # blocks
+        IDENTIFIER='ccaa00ddff2211de'.decode('hex'),
+        PREFIX='fd44a21f66ac7709'.decode('hex'),
+        P2P_PORT=8565,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9565,
+        BOOTSTRAP_ADDRS='doge.dtdns.net pool.hostv.pl rav3n.dtdns.net p2pool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 
 )
