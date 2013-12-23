9f0294cf739ebf785de0bc372c7efa64f79d720f
Rav3npl <rav3n.pl@gmail.com>
2013-12-24 00:24:27 +0100

    added CAT catcoin - experimental!

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -385,6 +385,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    catcoin=math.Object(
+        P2P_PREFIX='fcc1b7dc'.decode('hex'),
+        P2P_PORT=9333,
+        ADDRESS_VERSION=21,
+        RPC_PORT=9332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'catcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='CAT',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'CatCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Catcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.catcoin'), 'catcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://catcoin.net/block/', #dummy for now
+        ADDRESS_EXPLORER_URL_PREFIX='http://catcoin.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://catcoin.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
