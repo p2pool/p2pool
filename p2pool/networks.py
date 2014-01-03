5e3265ed207dc063b8c126384035ca1fdeb6143f
Rav3nPL <rav3n.pl@gmail.com>
2014-01-04 00:09:50 +0100

    Added GFT GiftCoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -430,6 +430,26 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    giftcoin=math.Object(
+        PARENT=networks.nets['giftcoin'],
+        SHARE_PERIOD=10,
+        CHAIN_LENGTH=12*60*60//10,
+        REAL_CHAIN_LENGTH=12*60*60//10,
+        TARGET_LOOKBEHIND=20,
+        SPREAD=50,
+        IDENTIFIER='f0e77e5ea777f087'.decode('hex'),
+        PREFIX='aafe077ab57af772'.decode('hex'),
+        P2P_PORT=8777,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9777,
+        BOOTSTRAP_ADDRS='rav3n.dtdns.net p2pool.gotgeeks.com p2pool.dndns.net solidpool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
+
+
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
