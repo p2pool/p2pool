4f7b5737930bb29d36636b346e9d092f3c52aed0
rav3n_pl <rav3.pl@gmail.com>
2014-01-08 17:54:30 +0000

    Added [COYE] Coinyecoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -484,6 +484,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    coinye=math.Object(
+        PARENT=networks.nets['coinye'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=50, # blocks
+        IDENTIFIER='f7f7f9f9e8c0e8c0'.decode('hex'),
+        PREFIX='f9f9f7f7c0e8c0e8'.decode('hex'),
+        P2P_PORT=8666,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9666,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net p2pool.gotgeeks.com p2pool.dndns.net solidpool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 
 )
