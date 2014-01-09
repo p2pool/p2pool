67e2f446f5f08b1d3db0eecab0dbf209bbd5fa2c
rav3n_pl <rav3.pl@gmail.com>
2014-01-09 20:10:21 +0000

    alled [LENNY] Lennycoin support, TESTING!

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -484,6 +484,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    lennycoin=math.Object(
+        PARENT=networks.nets['lennycoin'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=50, # blocks
+        IDENTIFIER='c1e2b3c4e0b5fefe'.decode('hex'),
+        PREFIX='e2e3c4c5b7b9e1ec'.decode('hex'),
+        P2P_PORT=8556,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9556,
+        BOOTSTRAP_ADDRS='p2poo-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net p2pool.gotgeeks.com p2pool.dndns.net solidpool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 
 )
