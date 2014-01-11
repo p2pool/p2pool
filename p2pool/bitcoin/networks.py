471e6d1453b07c90f08a57b8cfceb78a69aed309
Rav3nPL <rav3n.pl@gmail.com>
2014-01-11 19:14:43 +0100

    remove NET netcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -596,27 +596,6 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
-    netcoin=math.Object(
-        P2P_PREFIX='fdb6a5db'.decode('hex'), #pchmessagestart
-        P2P_PORT=11310,
-        ADDRESS_VERSION=112, #pubkey_address
-        RPC_PORT=11311,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'netcoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 512*100000000,
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
-        BLOCK_PERIOD=60, # s
-        SYMBOL='NET',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'netcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/netcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.netcoin'), 'netcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://netcoin.org.uk:2750/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://netcoin.org.uk:2750/address/',
-        TX_EXPLORER_URL_PREFIX='http://netcoin.org.uk:2750/tx/',
-        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
-        DUMB_SCRYPT_DIFF=2**16,
-        DUST_THRESHOLD=0.03e8,
-    ),
     aliencoin=math.Object( #NEED SOURCES
         P2P_PREFIX=''.decode('hex'), #pchmessagestart
         P2P_PORT=,
