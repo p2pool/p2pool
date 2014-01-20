69aadc35e93a42807f22d95c3eb91f19da854689
Rav3nPL <rav3n.pl@gmail.com>
2014-01-20 01:02:35 +0100

    added RPC ronpaulcoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -556,6 +556,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    rpcoin=math.Object(
+        PARENT=networks.nets['rpcoin'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=50, # blocks
+        IDENTIFIER='faf1e34fb0c7ef21'.decode('hex'),
+        PREFIX='e0ff56a1340e56f1'.decode('hex'),
+        P2P_PORT=8127,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9127,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net p2pool.gotgeeks.com p2pool.dndns.net solidpool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
