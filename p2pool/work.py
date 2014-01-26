df6ae09c5f39f3168f0c3444b89fae2f41be0568
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-01-26 09:33:37 -0700

    fix subsidy problem ii: electric dogealoo

--- p2pool/work.py
+++ p2pool/work.py
@@ -100,7 +100,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
                     transactions=[],
                     transaction_fees=[],
                     merkle_link=bitcoin_data.calculate_merkle_link([None], 0),
-                    subsidy=self.node.net.PARENT.SUBSIDY_FUNC(self.node.bitcoind_work.value['height']),
+                    subsidy=self.node.bitcoind_work.value['subsidy'],
                     last_update=self.node.bitcoind_work.value['last_update'],
                 )
             
