5a71922a7a352f8b787448a0c119bfccd507b6fe
Rav3nPL <rav3n.pl@gmail.com>
2014-01-27 08:36:12 +0100

    added MEOW kitthecoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -664,6 +664,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    kittehcoin=math.Object(
+        PARENT=networks.nets['kittehcoin'],
+        SHARE_PERIOD=5, # seconds
+        CHAIN_LENGTH=12*60*60//5, # shares
+        REAL_CHAIN_LENGTH=12*60*60//5, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=50, # blocks
+        IDENTIFIER='c0c1c2e3e6e9f1aa'.decode('hex'),
+        PREFIX='f1c3e9a023e0f077'.decode('hex'),
+        P2P_PORT=8566,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9566,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net p2pool.gotgeeks.com p2pool.dtdns.net solidpool.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
