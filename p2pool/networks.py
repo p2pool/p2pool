c027d4733ee5be2af4aa6db6b7dcf9ff05cd43d7
Rav3nPL <rav3n.pl@gmail.com>
2014-02-09 00:50:11 +0100

    added TGC TigerCoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -717,6 +717,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    tigercoin=math.Object(
+        PARENT=networks.nets['tigercoin'],
+        SHARE_PERIOD=5, # seconds
+        CHAIN_LENGTH=12*60*60//5, # shares
+        REAL_CHAIN_LENGTH=12*60*60//5, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=50, # blocks
+        IDENTIFIER='b5fafab5dbfdfdd5'.decode('hex'),
+        PREFIX='d5fddf5bb5faaffd'.decode('hex'),
+        P2P_PORT=8660,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**32 - 1,
+        PERSIST=False,
+        WORKER_PORT=9660,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net doge.dtdns.net pool.hostv.pl p2pool.org p2pool.gotgeeks.com p2pool.dtdns.net solidpool.org japool.com'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
