d8eaa73d5ec4727d4c13ece96431ab3aea558e20
Rav3nPL <rav3n.pl@gmail.com>
2014-02-09 21:24:25 +0100

    added FRY Frycoin support

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -735,6 +735,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    frycoin=math.Object(
+        PARENT=networks.nets['frycoin'],
+        SHARE_PERIOD=10, # seconds target spacing
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares coinbase maturity
+        SPREAD=50, # blocks
+        IDENTIFIER='fd467279436f696e'.decode('hex'),
+        PREFIX='fe436f696e465259'.decode('hex'),
+        P2P_PORT=8900,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9900,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net doge.dtdns.net pool.hostv.pl p2pool.org p2pool.gotgeeks.com p2pool.dtdns.net solidpool.org taken.pl'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
