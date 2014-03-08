8d9a88e884f01ecf32d30022d235222323292b93
squidcafe <clinton@squidcafe.com>
2014-03-08 11:31:04 -0500

    changed fckbanks explorer url
    changed the previous fckbanks explorer url to the new official one

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -1012,9 +1012,9 @@ nets = dict(
         BLOCK_PERIOD=60, # one block generation time
         SYMBOL='FCK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'fckbankscoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/fckbankscoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.fckbankscoin'), 'fckbankscoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://fckexplorer.hashattack.com:8080/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://fckexplorer.hashattack.com:8080/address/',
-        TX_EXPLORER_URL_PREFIX='http://fckexplorer.hashattack.com:8080/tx/',
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.fckbanks.org/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.fckbanks.org/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.fckbanks.org/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1), #??
         DUMB_SCRYPT_DIFF=2**16, #??
         DUST_THRESHOLD=0.03e8, #??
