b3623d8192651bdaad45018dead310c7bc0c56b6
terracoin <terracoin76@gmail.com>
2014-06-06 10:24:37 +0200

    new Terracoin network parameters.
     - SHARE_PERIOD changed from 30 to 60 seconds. Should give more time to
       miners to actually work on jobs.
     - SPREAD changed from 15 to 3. One share will pay over a smaller amount
       of blocks but should reduce share difficulty.
     - TARGET_LOOKBEHIND updated according to the new SHARE_PERIOD value,
       also slightly decreased so the overall share diff should adjust
       faster according to effective network  hashrate
     - IDENTIFIER & PREFIX values updated to prevent confusion between
       different networks.
    
     - when combining new SHARE_PERIOD & new SPREAD values, average share
       difficulty should be lowered (roughly * 0.4 ?)

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -86,32 +86,32 @@ nets = dict(
 
     terracoin=math.Object(
         PARENT=networks.nets['terracoin'],
-        SHARE_PERIOD=30, # seconds
+        SHARE_PERIOD=60, # seconds
         CHAIN_LENGTH=24*60*60//30, # shares
         REAL_CHAIN_LENGTH=24*60*60//30, # shares
-        TARGET_LOOKBEHIND=200, # shares
-        SPREAD=15, # blocks
-        IDENTIFIER='a41b2356a1b7d46e'.decode('hex'),
-        PREFIX='5623b62178d2b9b3'.decode('hex'),
+        TARGET_LOOKBEHIND=60, # shares
+        SPREAD=3, # blocks
+        IDENTIFIER='a42a285da1b7d46e'.decode('hex'),
+        PREFIX='5623762178d2a9b4'.decode('hex'),
         P2P_PORT=9323,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**32 - 1,
         PERSIST=True,
         WORKER_PORT=9322,
-        BOOTSTRAP_ADDRS='seed1.p2pool.terracoin.org seed2.p2pool.terracoin.org seed3.p2pool.terracoin.org forre.st vps.forre.st 93.97.192.93 66.90.73.83 67.83.108.0 219.84.64.174 24.167.17.248 109.74.195.142 83.211.86.49 94.23.34.145 168.7.116.243 94.174.40.189:9344 89.79.79.195 portals94.ns01.us'.split(' '),
+        BOOTSTRAP_ADDRS='seed1.p2pool.terracoin.org seed2.p2pool.terracoin.org seed3.p2pool.terracoin.org forre.st vps.forre.st 93.97.192.93 66.90.73.83 67.83.108.0 219.84.64.174 24.167.17.248 109.74.195.142 83.211.86.49 94.23.34.145 168.7.116.243 94.174.40.189:9344 89.79.79.195 portals94.ns01.us p2pool.org'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: 80002 <= v,
         VERSION_WARNING=lambda v: 'Upgrade Terracoin to >= 0.8.0.4!' if v < 80004 else None,
     ),
     terracoin_testnet=math.Object(
         PARENT=networks.nets['terracoin_testnet'],
-        SHARE_PERIOD=30, # seconds
+        SHARE_PERIOD=60, # seconds
         CHAIN_LENGTH=60*60//30, # shares
         REAL_CHAIN_LENGTH=60*60//30, # shares
-        TARGET_LOOKBEHIND=200, # shares
-        SPREAD=15, # blocks
-        IDENTIFIER='b41b2356a5b7d35d'.decode('hex'),
-        PREFIX='1623b92172d2b8a2'.decode('hex'),
+        TARGET_LOOKBEHIND=60, # shares
+        SPREAD=3, # blocks
+        IDENTIFIER='b41a2726a5b2d35d'.decode('hex'),
+        PREFIX='16d3b96182d2b8a3'.decode('hex'),
         P2P_PORT=19323,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**32 - 1,
