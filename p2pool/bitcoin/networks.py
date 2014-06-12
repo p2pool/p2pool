475c513069b1641c0e9929dc403c85b5a3c27869
Rav3nPL <rav3n.pl@gmail.com>
2014-06-12 18:08:53 +0200

    Added e-currency [ISO] support

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -1042,6 +1042,28 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    ecurrency=math.Object(
+        P2P_PREFIX='336b59c1'.decode('hex'),
+        P2P_PORT=8080,
+        ADDRESS_VERSION=0,
+        RPC_PORT=8079,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue('ecurrencyaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 100*100000000 >> (height + 1)//450000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=30, # s
+        SYMBOL='ISO',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'ecurrency') if platform.system() == 'Windows' 
+				else os.path.expanduser('~/Library/Application Support/ecurrency/') if platform.system() == 'Darwin' 
+				else os.path.expanduser('~/.ecurrency'), 'ecurrency.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://ecurrency.net/block/', #dummy
+        ADDRESS_EXPLORER_URL_PREFIX='http://ecurrency.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://ecrurrency.net/tx/',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -881,6 +881,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    ecurrency=math.Object(
+        PARENT=networks.nets['ecurrency'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares
+        SPREAD=20, # blocks
+        IDENTIFIER='f0e41ab70eff1e12'.decode('hex'),
+        PREFIX='e01fa73e0af1e40a'.decode('hex'),
+        P2P_PORT=8179,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**32 - 1,
+        PERSIST=False,
+        WORKER_PORT=9179,
+        BOOTSTRAP_ADDRS='rav3n.dtdns.net pool.hostv.pl p2pool.org solidpool.org taken.pl'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
