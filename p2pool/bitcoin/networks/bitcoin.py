3c9a813d8b7557713b94b2c6617f8183923ed6f4
Justin Ramos <jramos@changehealthcare.com>
2014-08-12 02:47:49 +0000

    porting 9dd70981e0e49e59a3c1ac852dd0a4a321cc340c

--- p2pool/bitcoin/networks/bitcoin.py
+++ p2pool/bitcoin/networks/bitcoin.py
@@ -23,6 +23,6 @@ CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitco
 BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
 TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
-SANE_TARGET_RANGE = (2**256//2**32//131072 - 1, 2**256//2**32 - 1)
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
