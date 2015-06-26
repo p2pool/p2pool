ab7cfb5aab898bcb77695534c8a3a14c1b7c5866
Forrest Voight <forrest@forre.st>
2015-06-26 19:22:40 -0400

    allow production of version 3 blocks

--- p2pool/work.py
+++ p2pool/work.py
@@ -369,7 +369,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
         self.last_work_shares.value[bitcoin_data.pubkey_hash_to_address(pubkey_hash, self.node.net.PARENT)]=share_info['bits']
         
         ba = dict(
-            version=min(self.current_work.value['version'], 2),
+            version=min(self.current_work.value['version'], 3),
             previous_block=self.current_work.value['previous_block'],
             merkle_link=merkle_link,
             coinb1=packed_gentx[:-self.COINBASE_NONCE_LENGTH-4],
