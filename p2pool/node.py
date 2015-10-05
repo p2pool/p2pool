2e5e25420e488bc1ad6816a0cc772fde88e0e07c
Forrest Voight <forrest@forre.st>
2015-10-04 22:51:15 -0400

    (modified patch from Jonathan Toomim) Performance fix: added methods to add or remove transactions to known_txs_var and trigger callbacks (for caching) on just the changes, instead of triggering a callback on the before and after state. This should improve performance dramatically with large mempools when receiving or removing transactions.

--- p2pool/node.py
+++ p2pool/node.py
@@ -40,9 +40,7 @@ class P2PNode(p2p.Node):
             
             self.node.tracker.add(share)
         
-        new_known_txs = dict(self.node.known_txs_var.value)
-        new_known_txs.update(all_new_txs)
-        self.node.known_txs_var.set(new_known_txs)
+        self.node.known_txs_var.add(all_new_txs)
         
         if new_count:
             self.node.set_best_share()
@@ -221,7 +219,7 @@ class Node(object):
         
         # BEST SHARE
         
-        self.known_txs_var = variable.Variable({}) # hash -> tx
+        self.known_txs_var = variable.VariableDict({}) # hash -> tx
         self.mining_txs_var = variable.Variable({}) # hash -> tx
         self.get_height_rel_highest = yield height_tracker.get_height_rel_highest_func(self.bitcoind, self.factory, lambda: self.bitcoind_work.value['previous_block'], self.net)
         
@@ -236,18 +234,18 @@ class Node(object):
         @self.bitcoind_work.changed.run_and_watch
         def _(_=None):
             new_mining_txs = {}
-            new_known_txs = dict(self.known_txs_var.value)
+            added_known_txs = {}
             for tx_hash, tx in zip(self.bitcoind_work.value['transaction_hashes'], self.bitcoind_work.value['transactions']):
                 new_mining_txs[tx_hash] = tx
-                new_known_txs[tx_hash] = tx
+                added_known_txs[tx_hash] = tx
             self.mining_txs_var.set(new_mining_txs)
-            self.known_txs_var.set(new_known_txs)
+            self.known_txs_var.add(added_known_txs)
         # add p2p transactions from bitcoind to known_txs
         @self.factory.new_tx.watch
         def _(tx):
-            new_known_txs = dict(self.known_txs_var.value)
-            new_known_txs[bitcoin_data.hash256(bitcoin_data.tx_type.pack(tx))] = tx
-            self.known_txs_var.set(new_known_txs)
+            self.known_txs_var.add({
+                bitcoin_data.hash256(bitcoin_data.tx_type.pack(tx)): tx,
+            })
         # forward transactions seen to bitcoind
         @self.known_txs_var.transitioned.watch
         @defer.inlineCallbacks
