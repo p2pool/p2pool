b23afa45b002e1e607da9b631c39a895c5ad8b92
Joseph W Becher <jwbecher@drazisil.com>
2015-11-02 23:47:45 -0500

    Saving before checking master

--- p2pool/bitcoin/p2p.py
+++ p2pool/bitcoin/p2p.py
@@ -15,7 +15,7 @@ from p2pool.util import deferral, p2protocol, pack, variable
 class Protocol(p2protocol.Protocol):
     def __init__(self, net):
         p2protocol.Protocol.__init__(self, net.P2P_PREFIX, 1000000, ignore_trailing_payload=True)
-    
+
     def connectionMade(self):
         if p2pool.DEBUG:
             print >>sys.stdout, 'Bitcoin connection made, sending version...'
@@ -38,7 +38,7 @@ class Protocol(p2protocol.Protocol):
             sub_version_num='/P2Pool:%s/' % (p2pool.__version__,),
             start_height=0,
         )
-    
+
     message_version = pack.ComposedType([
         ('version', pack.IntType(32)),
         ('services', pack.IntType(64)),
@@ -53,22 +53,22 @@ class Protocol(p2protocol.Protocol):
         if p2pool.DEBUG:
             print >>sys.stdout, 'Bitcoin connection made, handling version...'
         self.send_verack()
-    
+
     message_verack = pack.ComposedType([])
     def handle_verack(self):
         if p2pool.DEBUG:
             print >>sys.stdout, 'Bitcoin connection made, handling version ack...'
         self.get_block = deferral.ReplyMatcher(lambda hash: self.send_getdata(requests=[dict(type='block', hash=hash)]))
         self.get_block_header = deferral.ReplyMatcher(lambda hash: self.send_getheaders(version=1, have=[], last=hash))
-        
+
         if hasattr(self.factory, 'resetDelay'):
             self.factory.resetDelay()
         if hasattr(self.factory, 'gotConnection'):
             self.factory.gotConnection(self)
-        
+
         self.pinger = deferral.RobustLoopingCall(self.send_ping, nonce=1234)
         self.pinger.start(30)
-    
+
     message_inv = pack.ComposedType([
         ('invs', pack.ListType(pack.ComposedType([
             ('type', pack.EnumType(pack.IntType(32), {1: 'tx', 2: 'block'})),
@@ -83,7 +83,7 @@ class Protocol(p2protocol.Protocol):
                 self.factory.new_block.happened(inv['hash'])
             else:
                 print 'Unknown inv type', inv
-    
+
     message_getdata = pack.ComposedType([
         ('requests', pack.ListType(pack.ComposedType([
             ('type', pack.EnumType(pack.IntType(32), {1: 'tx', 2: 'block'})),
@@ -101,7 +101,7 @@ class Protocol(p2protocol.Protocol):
         ('last', pack.PossiblyNoneType(0, pack.IntType(256))),
     ])
     message_getaddr = pack.ComposedType([])
-    
+
     message_addr = pack.ComposedType([
         ('addrs', pack.ListType(pack.ComposedType([
             ('timestamp', pack.IntType(32)),
@@ -111,13 +111,13 @@ class Protocol(p2protocol.Protocol):
     def handle_addr(self, addrs):
         for addr in addrs:
             pass
-    
+
     message_tx = pack.ComposedType([
         ('tx', bitcoin_data.tx_type),
     ])
     def handle_tx(self, tx):
         self.factory.new_tx.happened(tx)
-    
+
     message_block = pack.ComposedType([
         ('block', bitcoin_data.block_type),
     ])
@@ -125,7 +125,7 @@ class Protocol(p2protocol.Protocol):
         block_hash = bitcoin_data.hash256(bitcoin_data.block_header_type.pack(block['header']))
         self.get_block.got_response(block_hash, block)
         self.get_block_header.got_response(block_hash, block['header'])
-    
+
     message_headers = pack.ComposedType([
         ('headers', pack.ListType(bitcoin_data.block_type)),
     ])
@@ -134,26 +134,26 @@ class Protocol(p2protocol.Protocol):
             header = header['header']
             self.get_block_header.got_response(bitcoin_data.hash256(bitcoin_data.block_header_type.pack(header)), header)
         self.factory.new_headers.happened([header['header'] for header in headers])
-    
+
     message_ping = pack.ComposedType([
         ('nonce', pack.IntType(64)),
     ])
     def handle_ping(self, nonce):
         self.send_pong(nonce=nonce)
-    
+
     message_pong = pack.ComposedType([
         ('nonce', pack.IntType(64)),
     ])
     def handle_pong(self, nonce):
         pass
-    
+
     message_alert = pack.ComposedType([
         ('message', pack.VarStrType()),
         ('signature', pack.VarStrType()),
     ])
     def handle_alert(self, message, signature):
         pass # print 'ALERT:', (message, signature)
-    
+
     def connectionLost(self, reason):
         if hasattr(self.factory, 'gotConnection'):
             self.factory.gotConnection(None)
@@ -164,28 +164,28 @@ class Protocol(p2protocol.Protocol):
 
 class ClientFactory(protocol.ReconnectingClientFactory):
     protocol = Protocol
-    
+
     maxDelay = 1
-    
+
     def __init__(self, net):
         self.net = net
         self.conn = variable.Variable(None)
-        
+
         self.new_block = variable.Event()
         self.new_tx = variable.Event()
         self.new_headers = variable.Event()
-    
+
     def buildProtocol(self, addr):
         p = self.protocol(self.net)
         p.factory = self
         return p
-    
+
     def gotConnection(self, conn):
         if p2pool.DEBUG:
             print >>sys.stdout, 'connection made, setting connection true.'
         self.conn.set(conn)
-    
+
     def getProtocol(self):
         if p2pool.DEBUG:
-            print >>sys.stdout, 'Checking connection status: %s', self.conn.get_not_none()
+            print >>sys.stdout, 'Checking connection status: ', self.conn.get_not_none()
         return self.conn.get_not_none()
