58726f19c3ea8a453759f9d6fd6dd3fc4fe0a501
Joseph W Becher <jwbecher@drazisil.com>
2015-11-17 00:17:27 -0500

    Fixed args

--- p2pool/test/test_p2p.py
+++ p2pool/test/test_p2p.py
@@ -29,7 +29,7 @@ class Test(unittest.TestCase):
         n = MyNode(df)
         n.start()
         # attempt to create shares
-        print subprocess.call(["bitcoin-cli", "-regtest setgenerate true 10"])
+        print subprocess.call(["bitcoin-cli", "-regtest", "setgenerate", "true", "10"])
         try:
             yield df
         except:
