643768f7328a6fb6d1266f943e78cb8df65aa9a1
Jonathan Toomim <j@toom.im>
2017-04-03 13:10:08 -0700

    Increase limit on new transactions in a share from 50kB to 1000kB to fix issue #341.

--- p2pool/data.py
+++ p2pool/data.py
@@ -149,7 +149,7 @@ class NewShare(object):
             else:
                 if known_txs is not None:
                     this_size = bitcoin_data.tx_type.packed_size(known_txs[tx_hash])
-                    if new_transaction_size + this_size > 50000: # only allow 50 kB of new txns/share
+                    if new_transaction_size + this_size > 1000000: # only allow 1000 kB of new txns/share
                         break
                     new_transaction_size += this_size
                 new_transaction_hashes.append(tx_hash)
@@ -369,7 +369,7 @@ class NewShare(object):
                 return True, 'txs over block size limit'
             
             new_txs_size = sum(bitcoin_data.tx_type.packed_size(known_txs[tx_hash]) for tx_hash in self.share_info['new_transaction_hashes'])
-            if new_txs_size > 50000:
+            if new_txs_size > 1000000:
                 return True, 'new txs over limit'
         
         return False, None
