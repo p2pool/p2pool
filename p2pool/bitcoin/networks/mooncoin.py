ec79eafba0938f36ad8c14a602908ba2461a993a
Rafał Kiełbus <rafal.kielbus@akcelerator.tech>
2017-12-18 19:11:33 +0100

    swap files

--- p2pool/bitcoin/networks/mooncoin.py
+++ p2pool/bitcoin/networks/mooncoin.py
@@ -1,19 +1,28 @@
-from p2pool.bitcoin import networks
+import os
+import platform
 
-PARENT = networks.nets['mooncoin']
-SHARE_PERIOD = 25 # seconds target spacing
-CHAIN_LENGTH = 12*60*60//25 # shares
-REAL_CHAIN_LENGTH = 12*60*60//25 # shares
-TARGET_LOOKBEHIND = 20 # shares coinbase maturity
-SPREAD = 10 # blocks
-IDENTIFIER = 'e8e8c0c0f7f7f9f9'.decode('hex')
-PREFIX = 'c0c0e8e8f7f7f9f9'.decode('hex')
-P2P_PORT = 8664
-MIN_TARGET = 0
-MAX_TARGET = 2**256//2**20 - 1
-PERSIST = False
-WORKER_PORT = 9664
-BOOTSTRAP_ADDRS = 'rav3n.dtdns.net pool.hostv.pl p2pool.org solidpool.org'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool-alt'
-VERSION_CHECK = lambda v: True
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'f9f7c0e8'.decode('hex')
+P2P_PORT = 44664
+ADDRESS_VERSION = 3
+RPC_PORT = 44663
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            (yield helper.check_genesis_block(bitcoind, 'bd687cbbac4842724d4b1dfd2e1a6ce35d765db05635b3ebd3810ba66ac5aee9'))
+            ))
+SUBSIDY_FUNC = lambda height: 2000000*100000000
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+BLOCK_PERIOD = 90 # s
+SYMBOL = 'MOON'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'MoonCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Mooncoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Mooncoin'), 'Mooncoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'http://moonchain.net/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://moonchain.net/address/'
+TX_EXPLORER_URL_PREFIX = 'http://moonchain.net/tx/'
+SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
+DUMB_SCRYPT_DIFF = 2**16
+DUST_THRESHOLD = 0.03e8
 
